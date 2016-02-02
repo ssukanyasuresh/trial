822d41a0cad51a5753959d224c99180754e05bbc
suku1994 <ssukanya1994@gmail.com>
2016-02-02 11:56:55 +0530

    [sukanya/rebecca]
    Removes Name and Address getters from Order

---
+++ Documents/tw/Refactoring_Techniques_1/src/com/twu/refactoring/Order.java
@@ -0,0 +1,23 @@
+package com.twu.refactoring;
+
+import java.util.List;
+
+public class Order {
+    String name;
+    String address;
+    List<LineItem> lineItems;
+
+    public Order(String name, String address, List<LineItem> lineItems) {
+        this.name = name;
+        this.address = address;
+        this.lineItems = lineItems;
+    }
+
+    public List<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public String printCustomerDetails() {
+        return this.name + "\n" + this.address;
+    }
+}
